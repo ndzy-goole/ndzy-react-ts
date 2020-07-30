cbcf4418ca29b0a44c350ef9fda17a1743f97f4e
张一 <2326962137@qq.com>
2020-07-30 13:21:05 +0800

    初始化项目

--- README.md
+++ README.md
@@ -4,3 +4,27 @@
 分支合并
 git merge --no-ff dev
 ```
+
+# node-sass 等
+
+yarn add react-app-rewired customize-cra babel-plugin-import less less-loader@5.0.0 node-sass
+
+# antd
+
+yarn add antd @ant-design/icons
+
+# 加入路由的支持
+
+yarn add react-router-dom @types/react-router-dom react-router
+
+# html 样式优化
+
+yarn add normalize.css
+
+# axios
+
+yarn add axios
+
+# ahooks
+
+yarn add ahooks
---
+++ config-overrides.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addWebpackAlias
+} = require('customize-cra');
+
+// const darkThemeVars = require("antd/dist/dark-theme");
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  // 路径别名
+  addWebpackAlias({
+    ['@components']: path.resolve(__dirname, 'src/components')
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      hack: `true;@import "${require.resolve(
+        'antd/lib/style/color/colorPalette.less'
+      )}";`
+      // ...darkThemeVars,
+      // "@primary-color": "#02b875"
+    },
+    localIdentName: '[local]--[hassh:base64:5]'
+  })
+);
